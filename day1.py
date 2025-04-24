c59567ec55da035363e552d4ae5ce1df6045849e
Satya prakash mishra <satyaprakashmishra23.12@gmail.com>
2025-04-24 20:04:05 +0530

    First baby steps

---
+++ day1.py
@@ -0,0 +1,27 @@
+# print("Hello World!!")
+
+# name="Satya"
+# age=24
+# height=6.2
+# is_Student=False
+#
+# print(name,age, height, is_Student)
+# print(type(height))
+
+
+# personalized introduction
+# # name = input("Enter your name: ")
+# # age = input("enter your age: ")
+# # print(f"Hello {name}! Your age is {age}.")
+# print(type(int(age)))
+
+
+# Sum, Product and Average
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the 2nd number: "))
+sum_result = num1 + num2
+product = num1*num2
+average = (num1+num2)/2;
+print(f"Sum of the nums are: {sum_result}")
+print(f"Product: {product}")
+print(f"And Average: {average}")
